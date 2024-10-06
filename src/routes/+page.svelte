60cde692983d7e74608bde19f9694c7e95247631
Daniel Laible <daniellaible@gmail.com>
2024-10-06 15:08:10 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+
+    onMount(()=>{
+        goto('/start');
+    })
+
+
+</script>
+
+
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme('colors.gray.100');
+    }
+</style>
